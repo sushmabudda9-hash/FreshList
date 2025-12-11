30066f63e92a06a8eec2a544ee29ab5b4640042c
sushmabudda9 <sushmabudda9@gmail.com>
2025-12-11 23:20:48 +0000

    Added Firebase dependencies to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,14 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.compose.material:material-icons-extended")
+
+
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("com.google.firebase:firebase-firestore")
 }
